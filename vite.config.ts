138ed702decdb621ede8173c203b15286ac8be52
Levon <levon.s.avetisyan@gmail.com>
2025-02-12 00:03:17 +0400

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/highonprofits/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 900, // Adjust this value if needed
+  },
 });
